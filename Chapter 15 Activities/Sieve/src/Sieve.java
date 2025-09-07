82b7ac8c834ec645d457ed8560a304e1c7d6b50e
Jonathan Yuan <142510555+JonathanYuan3@users.noreply.github.com>
2025-09-06 19:07:57 -0500

    PP: Sieve of Eratosthenes

--- Chapter 15 Activities/Sieve/src/Sieve.java
+++ Chapter 15 Activities/Sieve/src/Sieve.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
-
+import java.util.Set;
+import java.util.HashSet;
 
 /**
  * A program that implements the sieve of Eratosthenes.
@@ -11,9 +12,24 @@ public class Sieve
         Scanner in = new Scanner(System.in);
         System.out.println("Compute primes up to which integer?");
         int n = in.nextInt();
-
-        // Your work goes here
-        . . .
+        
+        // Compile a list of all the terms
+        Set<Integer> terms = new HashSet<>(); 
+        for(int i = 2; i <= n; i++) {
+            terms.add(i);
+        }
+        
+        // Perform Eratosthenes' sieve
+        for (int i = 2; i * i <= n; i++) {
+            if (terms.contains(i)) { //Check for a number that hasn't been removed (a prime number)
+                // Remove all multiples of i, excluding i itself
+                for (int j = 2 * i; j <= n; j += i) { //Removes all multiples of that number
+                    terms.remove(j);
+                }
+            }
+        }
+        
+        System.out.println(terms);
 
 
 
