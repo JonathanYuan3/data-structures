19a38e93c04d5a697b67d681b67d6bfd269239ec
“jyyuan <jyyuan@stu.naperville203.org>
2025-09-26 13:17:32 -0500

    9/26/2025 LinkedList

--- Chapter 16 Class Notes/src/ListDemo.java
+++ Chapter 16 Class Notes/src/ListDemo.java
@@ -5,5 +5,11 @@ public class ListDemo
 {
     public static void main(String[] args)
     {
+        LinkedList list = new LinkedList();
+        list.addFirst("One");
+        list.addFirst("Two");
+        list.addFirst("Three");
+        list.addFirst("Four");
+        System.out.println(list);
     }
 }
