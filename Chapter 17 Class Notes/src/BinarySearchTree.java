9203e97c528581d6dfdf8dea9e9f7e243f28a4a0
“jyyuan <jyyuan@stu.naperville203.org>
2025-11-05 13:35:11 -0600

    11/5 Binary Search Tree

--- Chapter 17 Class Notes/src/BinarySearchTree.java
+++ Chapter 17 Class Notes/src/BinarySearchTree.java
@@ -1,27 +1,34 @@
 /**
-    This class implements a binary search tree whose
-    nodes hold objects that implement the Comparable
-    interface.
-*/
-public class BinarySearchTree
-{   
+ * This class implements a binary search tree whose
+ * nodes hold objects that implement the Comparable
+ * interface.
+ */
+public class BinarySearchTree {
     private Node root;
 
     /**
-        Constructs an empty tree.
-    */
-    public BinarySearchTree()
-    {   
-        
+     * Constructs an empty tree.
+     */
+    public BinarySearchTree() {
+        this.root = null;
     }
-    
+
     /**
-        Inserts a new node into the tree.
-        @param obj the object to insert
-    */
-    public void add(Comparable obj) 
-    {   
-        
+     * Inserts a new node into the tree.
+     * 
+     * @param obj the object to insert
+     */
+    public void add(Comparable obj) {
+        Node newNode = new Node();
+        newNode.data = obj;
+        newNode.left = null;
+        newNode.right = null;
+
+        if (this.root == null) {
+            root = newNode;
+        } else {
+            this.root.addNode(newNode);
+        }
     }
 
     /**
@@ -31,54 +38,98 @@ public class BinarySearchTree
     */
     public boolean find(Comparable obj)
     {
+        Node current = this.root;
+        while(current != null) {
+            int diff = obj.compareTo(current.data);
+            if(diff == 0) {
+                return true;
+            }
+            else if (diff < 0) {
+                current = current.left;
+            }
+            else if (diff > 0) {
+                current = current.right;
+            }
+        }
         return false;
     }
-    
-    /**
-        Tries to remove an object from the tree. Does nothing
-        if the object is not contained in the tree.
-        @param obj the object to remove
-    */
-    public void remove(Comparable obj)
-    {
-        
-    }
-    
-    /**
-        Prints the contents of the tree in sorted order.
-    */
-    public void print()
-    {   
-        
-    }   
 
     /**
-        Prints a node and all of its descendants in sorted order.
-        @param parent the root of the subtree to print
-    */
-    private static void print(Node parent)
-    {   
-        
+     * Tries to remove an object from the tree. Does nothing
+     * if the object is not contained in the tree.
+     * 
+     * @param obj the object to remove
+     */
+    public void remove(Comparable obj) {
+        Node toBeRemoved = this.root;
+        boolean found = false;
+
+        while(!found && toBeRemoved != null) {
+            int diff = obj.compareTo(toBeRemoved.data);
+            if(diff == 0) {
+                found = true;
+            }
+            else if (diff < 0) {
+                toBeRemoved = toBeRemoved.left;
+            }
+            else if (diff > 0) {
+                toBeRemoved = toBeRemoved.right;
+            }
+        }
+
+        if(!found) {
+            return;
+        }
     }
 
     /**
-        A node of a tree stores a data item and references
-        to the left and right child nodes.
-    */
-    static class Node
-    {   
-        
+     * Prints the contents of the tree in sorted order.
+     */
+    public void print() {
+
+    }
+
+    /**
+     * Prints a node and all of its descendants in sorted order.
+     * 
+     * @param parent the root of the subtree to print
+     */
+    private static void print(Node parent) {
+
+    }
+
+    /**
+     * A node of a tree stores a data item and references
+     * to the left and right child nodes.
+     */
+    static class Node {
+        // A BST must be made of Comparable Objects
+        public Comparable data;
+        public Node left;
+        public Node right;
 
         /**
-            Inserts a new node as a descendant of this node.
-            @param newNode the node to insert
-        */
-        public void addNode(Node newNode)
-        {   
-            
+         * Inserts a new node as a descendant of this node.
+         * 
+         * @param newNode the node to insert
+         */
+        public void addNode(Node newNode) {
+            // If diff < 0, newNode is to the left of this node
+            // If diff > 0, newNode is to the right of this node
+            int diff = newNode.data.compareTo(data);
+            if (diff < 0) {
+                if (left == null) {
+                    left = newNode;
+                } else {
+                    left.addNode(newNode);
+                }
+            } else if (diff > 0) {
+                if (right == null) {
+                    right = newNode;
+                } else {
+                    right.addNode(newNode);
+                }
+            }
         }
     }
 }
-
-
-
